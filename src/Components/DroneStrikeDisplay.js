5540fe0a80e4856450d700e13d16be3d0bd3f4a8
Ryan Ghaida <ryaboug@gmail.com>
2018-07-09 11:35:52 -0400

    only display strikes when a strike is selected

--- src/Components/DroneStrikeDisplay.js
+++ src/Components/DroneStrikeDisplay.js
@@ -2,25 +2,29 @@ import React from 'react';
 
 const DroneStrikeDisplay = (props) => {
 
-
-
-
+    let formatDate = () => {
+        if(props.strike.date){
+        let longDate = props.strike.date;
+        let date = longDate.split("T")[0];
+        return date}
+    }
     
     return(
+        (props.strike !== "") ? 
         <div class='col-md-8'>
             <h1>Strike Details</h1>
         <div id="beer-detail">
-            <h3>{props.strike.date} </h3>
+            <h3>{formatDate()} </h3>
             <h2>{props.strike.narrative}</h2>
             <ul>
                 <h3>Deaths: {props.strike.deaths}</h3>
-                {(props.strike.injuries != "") ? <h3>Injuries: {props.strike.injuries}</h3> : <h3>No reported Injuries</h3>}
-                {(props.strike.civilians != "") ? <h3>Civilians: {props.strike.civilians}</h3> : <h3>No confimred civilian casualties</h3>}
-                {(props.strike.children != "") ? <h3>Children: {props.strike.children}</h3> : <h3>No confimred children casualties</h3>}
+                {(props.strike.injuries !== "") ? <h3>Injuries: {props.strike.injuries}</h3> : <h3>No reported Injuries</h3>}
+                {(props.strike.civilians !== "") ? <h3>Civilians: {props.strike.civilians}</h3> : <h3>No confimred civilian casualties</h3>}
+                {(props.strike.children !== "") ? <h3>Children: {props.strike.children}</h3> : <h3>No confimred children casualties</h3>}
                 
             </ul>
         </div>
-      </div>
+      </div> : null
     )
 }
 
