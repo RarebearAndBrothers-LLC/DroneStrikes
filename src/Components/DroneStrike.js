30c57ed09749497193fd145a9cda3686467e77ec
Ryan Ghaida <ryaboug@gmail.com>
2018-07-09 11:30:20 -0400

    fix date and add to titles

--- src/Components/DroneStrike.js
+++ src/Components/DroneStrike.js
@@ -5,9 +5,15 @@ import React from 'react';
 
 const DroneStrike = (props) => {
 
+   let formatDate = () => {
+        let longDate = props.strike.date;
+        let date = longDate.split("T")[0];
+        return date
+    }
+
     return (
         <div onClick={()=>props.handleClick(props.strike)}>
-           <h4>{props.strike.country}</h4>
+           <h4>{props.strike.country} | {formatDate()}</h4>
         </div>
     )
 }
--- src/Components/DroneStrikeList.js
+++ src/Components/DroneStrikeList.js
@@ -5,7 +5,6 @@ import DroneStrike from './DroneStrike';
 const DroneStrikeList = (props) => {
 
     const mapStrikes = () => {
-      
         return props.droneStrikes.map((s)=> {
             return <DroneStrike key={s.number}  strike={s} handleClick={props.handleClick}/> 
         })
