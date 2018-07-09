6aaa72dcff9d0a008c0a32d74e34210ca02c4f07
Ryan Ghaida <ryaboug@gmail.com>
2018-07-09 11:07:14 -0400

    Conditional Rendering for deaths and injuries data

--- src/Components/DroneStrikeDisplay.js
+++ src/Components/DroneStrikeDisplay.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
 const DroneStrikeDisplay = (props) => {
+
+
+
+
+    
     return(
         <div class='col-md-8'>
-        <h2>Strike Detail</h2>
+            <h1>Strike Details</h1>
         <div id="beer-detail">
-        <h1>{props.strike.narrative}</h1>
+            <h3>{props.strike.date} </h3>
+            <h2>{props.strike.narrative}</h2>
+            <ul>
+                <h3>Deaths: {props.strike.deaths}</h3>
+                {(props.strike.injuries != "") ? <h3>Injuries: {props.strike.injuries}</h3> : <h3>No reported Injuries</h3>}
+                {(props.strike.civilians != "") ? <h3>Civilians: {props.strike.civilians}</h3> : <h3>No confimred civilian casualties</h3>}
+                {(props.strike.children != "") ? <h3>Children: {props.strike.children}</h3> : <h3>No confimred children casualties</h3>}
+                
+            </ul>
         </div>
       </div>
     )
